a8081379af6ea8307ac538d216038946e0afccb7
chhhhhao <cnchuao@gmail.com>
2019-04-20 21:53:19 +0800

    beautify code

--- main.py
+++ main.py
@@ -3,17 +3,20 @@ import torchvision as tv
 import utils
 from modifiedVGG import Vgg16
 from torch.nn import functional as F
-class Config(object):
-    image_size=512
-    style_path='style_images/style.jpg'
-    content_path='content_images/content.jpg'
-    combined_path='combined_images'
 
-    content_weight=1
-    style_weight=1000
-    lr=1e-3
-    epoches=10000
+
+class Config(object):
+    image_size = 512
+    style_path = 'style_images/style.jpg'
+    content_path = 'content_images/content.jpg'
+    combined_path = 'combined_images'
+
+    content_weight = 1
+    style_weight = 1000
+    lr = 1e-3
+    epoches = 10000
     device = t.device("cuda" if t.cuda.is_available() else "cpu")
+
 
 def train():
     cfg = Config()
@@ -21,35 +24,45 @@ def train():
     for param in vgg.parameters():
         param.requires_grad = False
     # 固定网络的参数
-    content = utils.get_image_data(cfg.content_path,cfg.image_size).to(cfg.device)
-    style = utils.get_image_data(cfg.style_path,cfg.image_size).to(cfg.device)
+    content = utils.get_image_data(cfg.content_path,
+                                   cfg.image_size).to(cfg.device)
+    style = utils.get_image_data(cfg.style_path, cfg.image_size).to(cfg.device)
     target = content.clone().requires_grad_(True)
-
 
     content_features = vgg(content)
     style_features = vgg(style)
-    gram_styles = [utils.gram_matrix(x).requires_grad_(False) for x in style_features]
-    batches,channels,h,w=list(gram_styles.size())
-        # 注意要使style——gram的requires_grad置于False，F.mse_loss要求
-    optimizer = t.optim.Adam([target],lr=cfg.lr)
+    gram_styles = [
+        utils.gram_matrix(x).requires_grad_(False) for x in style_features
+    ]
+    batches, channels, h, w = list(gram_styles.size())
+    # 注意要使style——gram的requires_grad置于False，F.mse_loss要求
+    optimizer = t.optim.Adam([target], lr=cfg.lr)
     for epoch in range(cfg.epoches):
         target_features = vgg(target)
-        content_loss = F.mse_loss(target_features.relu3_3,content_features.relu3_3.requires_grad_(False))
+        content_loss = F.mse_loss(
+            target_features.relu3_3,
+            content_features.relu3_3.requires_grad_(False))
 
-        style_loss=0.
-        for tar,gram_style in zip(target_features,gram_styles):
+        style_loss = 0.
+        for tar, gram_style in zip(target_features, gram_styles):
             tar_gram = utils.gram_matrix(tar)
-            style_loss += F.mse_loss(tar_gram,gram_style)
-        style_loss = style_loss/(2*channels*h*w)^2
-        total_loss = cfg.content_weight*content_loss + cfg.style_weight*style_loss
+            style_loss += F.mse_loss(tar_gram, gram_style)
+        style_loss = style_loss / (2 * channels * h * w) ^ 2
+        total_loss = cfg.content_weight * content_loss + cfg.style_weight * style_loss
         optimizer.zero_grad()
         total_loss.backward()
         optimizer.step()
-        if epoch%100 == 0:
-            print("iteration:{}  Content loss:{:.4f},Style loss:{:.4f},Total loss:{:.4f}".format(epoch+1,content_loss.item(),style_loss.item(),total_loss.item()))
+        if epoch % 100 == 0:
+            print(
+                "iteration:{}  Content loss:{:.4f},Style loss:{:.4f},Total loss:{:.4f}"
+                .format(epoch + 1, content_loss.item(), style_loss.item(),
+                        total_loss.item()))
 
-    denorm = tv.transforms.Normalize([-2.12,-2.04,-1.80],[4.37,4.46,4.44])
-    target = denorm(target.squeeze().to('cpu')).clamp_(min = 0,max = 1)
-    tv.utils.save_image(target,cfg.combined_path + '/output '+str(cfg.content_weight/cfg.style_weight)+'.png')
+    denorm = tv.transforms.Normalize([-2.12, -2.04, -1.80], [4.37, 4.46, 4.44])
+    target = denorm(target.squeeze().to('cpu')).clamp_(min=0, max=1)
+    tv.utils.save_image(
+        target, cfg.combined_path + '/output ' +
+        str(cfg.content_weight / cfg.style_weight) + '.png')
+
 
 train()
--- utils.py
+++ utils.py
@@ -2,22 +2,26 @@ import torch as t
 import torchvision as tv
 import numpy as np
 
+
 def gram_matrix(y):
-    b,c,h,w = y.size()
-    feature = y.view(b,c,h*w)
-    gram = t.bmm(feature,feature.transpose(1,2))
+    b, c, h, w = y.size()
+    feature = y.view(b, c, h * w)
+    gram = t.bmm(feature, feature.transpose(1, 2))
     return gram
+
+
 imagenet_mean = [0.485, 0.456, 0.406]
 imagenet_std = [0.229, 0.224, 0.225]
 #  vgg16 是在imagenet 上训练的
 # 而imagenet的图像已被归一化为 mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225]
 
-def get_image_data(path,image_size):
-    image_transforms=tv.transforms.Compose([
-    tv.transforms.Resize(image_size),
-    tv.transforms.CenterCrop(image_size),
-    tv.transforms.ToTensor(),
-    tv.transforms.Normalize(mean=imagenet_mean,std=imagenet_std),
+
+def get_image_data(path, image_size):
+    image_transforms = tv.transforms.Compose([
+        tv.transforms.Resize(image_size),
+        tv.transforms.CenterCrop(image_size),
+        tv.transforms.ToTensor(),
+        tv.transforms.Normalize(mean=imagenet_mean, std=imagenet_std),
     ])
     image = tv.datasets.folder.default_loader(path)
     img_tensor = image_transforms(image).unsqueeze(0)
